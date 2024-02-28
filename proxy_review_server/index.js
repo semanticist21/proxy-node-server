0c73240d3bb3cb943c92195f2119d68bf5e84115
IMHOJEONG <39ghwjd@naver.com>
2024-02-28 09:02:06 +0900

    [add]: add new store-wp.mui.com proxy.server

---
+++ proxy_review_server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+var options = {
+  target: "https://store-wp.mui.com", // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+  },
+};
+
+app.use("/", createProxyMiddleware(options));
+
+app.listen(8084);
